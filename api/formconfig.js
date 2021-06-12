fcde7129b84c6c0eb7339c2bc4d0c348603a2a36
vencax <vencax@centrum.cz>
2021-06-12 14:37:32 +0200

    fix

--- api/formconfig.js
+++ api/formconfig.js
@@ -1,36 +1,38 @@
-export default [
-  {
-    name: 'username',
-    component: 'dyn-input',
-    fieldcomponent: true,
-    label: 'Uživatelské jméno',
-    rules: 'required'
-  },
-  {
-    name: 'name',
-    component: 'dyn-input',
-    fieldcomponent: true,
-    label: 'Jméno',
-    rules: 'required'
-  },
-  {
-    name: 'email',
-    component: 'dyn-input',
-    fieldcomponent: true,
-    label: 'Email'
-  },
-  {
-    name: 'status',
-    component: 'dyn-select',
-    options: [
-      { value: 0, text: 'OK' },
-      { value: 2, text: 'Zablokován' }
-    ],
-    fieldcomponent: true,
-    label: 'Status'
-  },
-  {
-    name: 'created',
-    fieldcomponent: true
-  }
-]
+export default {
+  attrs: [
+    {
+      name: 'username',
+      component: 'dyn-input',
+      fieldcomponent: true,
+      label: 'Uživatelské jméno',
+      rules: 'required'
+    },
+    {
+      name: 'name',
+      component: 'dyn-input',
+      fieldcomponent: true,
+      label: 'Jméno',
+      rules: 'required'
+    },
+    {
+      name: 'email',
+      component: 'dyn-input',
+      fieldcomponent: true,
+      label: 'Email'
+    },
+    {
+      name: 'status',
+      component: 'dyn-select',
+      options: [
+        { value: 0, text: 'OK' },
+        { value: 2, text: 'Zablokován' }
+      ],
+      fieldcomponent: true,
+      label: 'Status'
+    },
+    {
+      name: 'created',
+      fieldcomponent: true
+    }
+  ]
+}
